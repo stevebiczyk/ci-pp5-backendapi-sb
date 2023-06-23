64fe0f2cf6f8ceaed13b7866bf23c439022c5c2f
stevebiczyk <stevebiczyk@gmail.com>
2023-06-23 15:15:25 +0000

    added login/logout views, IsOwner permission and the is_owner field to ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -4,34 +4,47 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from pp5_backend_sb.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList (APIView):
-  def get(self, request):
-    profiles = Profile.objects.all()
-    serializer = ProfileSerializer(profiles, many=True)
-    return Response(serializer.data)
+class ProfileList(APIView):
+    """
+    List all profiles
+    No Create view (post method), as profile creation handled by django signals
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
-  serializer_class = ProfileSerializer
-  def get_object(self, pk):
-    try:
-      profile = Profile.objects.get(pk=pk)
-      return profile
-    except Profile.DoesNotExist:
-      raise Http404
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
-  def get(self, request, pk):
-    profile = self.get_object(pk)
-    serializer = ProfileSerializer(profile)
-    return Response(serializer.data)
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
 
-  def put(self, request, pk):
-    profile = self.get_object(pk)
-    serializer = ProfileSerializer(profile, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile, context={'request': request}
+        )
+        return Response(serializer.data)
 
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
