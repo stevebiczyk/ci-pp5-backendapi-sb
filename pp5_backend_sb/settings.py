1a2ffaa0e99bf1a825a12ab0fa0fd63a32f34d6c
stevebiczyk <stevebiczyk@gmail.com>
2023-07-03 09:48:40 +0000

    Heroku error fix

--- pp5_backend_sb/settings.py
+++ pp5_backend_sb/settings.py
@@ -153,7 +153,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL").encode())
     }
     # print('connected')
 
