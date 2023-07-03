186bed9ebd3539ece2423c664881405b30bb5298
stevebiczyk <stevebiczyk@gmail.com>
2023-07-03 10:14:26 +0000

    Heroku error fix

--- pp5_backend_sb/settings.py
+++ pp5_backend_sb/settings.py
@@ -65,7 +65,9 @@ DEBUG = 'DEV' in os.environ
 
 # ALLOWED_HOSTS = ['8000-stevebiczyk-cipp5backen-4go3zwdljg4.ws-eu100.gitpod.io']
 # ALLOWED_HOSTS = ['8000-stevebiczyk-cipp5backen-4go3zwdljg4.ws-eu101.gitpod.io']
-ALLOWED_HOSTS = ['localhost', 'ci-pp5-backendapi-sb.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'ci-pp5-backendapi-sb-1f7f0e4a782e.herokuapp.com']
+
+
 
 
 # Application definition
